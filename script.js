de4662ea18959917acd0bb8b4fe5d825ca6aba28
Vishesh verma <119738589+cbsewalechicha10@users.noreply.github.com>
2024-12-12 09:15:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMore() {
-    const moreInfo = document.getElementById('more-info');
-    if (moreInfo.classList.contains('hidden')) {
-        moreInfo.classList.remove('hidden');
-        moreInfo.classList.add('fade-in');
-    } else {
-        moreInfo.classList.add('hidden');
-    }
-}
